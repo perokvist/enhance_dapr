f413a6d8c259de680b2e79be7d5d8e0aa69a7150
Per Ökvist <perokvist@users.noreply.github.com>
2023-02-21 17:26:27 +0100

    dapr SDK

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # enhance_dapr
+
+Containerized enhance app with dapr SDK starting point. WiP
+
+```bash
+dapr run --app-id enhance --dapr-http-port 3500 -- npm start
+```
--- src/web/app/api/index.mjs
+++ src/web/app/api/index.mjs
@@ -1,6 +1,31 @@
+import { DaprClient } from "@dapr/dapr";
+
+const daprHost = "127.0.0.1"; // Dapr Sidecar Host
+const daprPort = "3500"; // Dapr Sidecar Port of this Example Server
+const serviceStoreName = "statestore";
+
+const client = new DaprClient(daprHost, daprPort);
+
 export async function get (req) {
   console.log(req)
+
+  // Save State
+  const _ = await client.state.save(serviceStoreName, [
+    {
+      key: "first-key-name",
+      value: "hello",
+    },
+    {
+      key: "second-key-name",
+      value: "world",
+    },
+  ]);
+
+
+  // Get State
+  const response = await client.state.get(serviceStoreName, "first-key-name");
+
   return {
-    json: ['one', 2, true]
+    json: [response]
   }
 }
